df0a1141f57d9b8c9c0d12778f5f63f6fe0b98db
russm <russmaloneunlimited@gmail.com>
2024-12-02 17:34:04 -0500

    Integrated bfs and dfs into the AdjList class, made appropriate changes to do so

--- BeautifulSoup/breadthFirstSearch.py
+++ BeautifulSoup/breadthFirstSearch.py
@@ -6,31 +6,41 @@ import timeit
 def bfs(links, start, target):
     queue = deque([start])
     visited = set()
+    #Stores every URL that goes from the start URL to the url acting as a key
     paths = {start : f"{start}"}
     result = bfs_helper(links, start, target, queue, visited, paths)
     if result:
+        #Prints out the shortest path from the start URL to the final URL
+        print(f"Breadth First Search:")
         print(f"The shortest path between {start} and {target} is: {paths[target]}")
+        #Prints out the average of the BFS function running 100 times
         t = timeit.Timer(lambda: bfs_helper(links, start, target, queue, visited, paths))
-        print("Total time taken: ", t.timeit(100), "seconds")
+        print("Total time taken for BFS: ", t.timeit(100), "seconds")
     else:
+        #Reports if the search was unsuccessful, as well as the average time of the function
+        print(f"Breadth First Search Failed:")
         print(f"There is no path between {start} and {target}")
         t = timeit.Timer(lambda: bfs_helper(links, start, target, queue, visited, paths))
-        print("Total time taken: ", t.timeit(100), "seconds")
+        print("Total time taken for BFS: ", t.timeit(100), "seconds")
 
 def bfs_helper(links, start, target, queue, visited, paths):
     visited.add(start)
     while len(queue) != 0:
+        #During each loop, the URL at the front of the queue is removed
         current = queue.popleft()
-
+        #Checks if any of the URLs that the current URL links to is the final URL
         for neighbor in links[current]:
             if neighbor == target:
-
+                #Records the urls that lead up to the final URL from the start URL in the paths dictionary
                 paths[target] = paths[current] + f" -> {neighbor}"
                 return True
             if neighbor not in visited:
-                queue.append(neighbor)
-                visited.add(neighbor)
-                paths[neighbor] = paths[current] + f" -> {neighbor}"
+                #Every other URL is added to the queue, marked as a visited URL, and has the path leading
+                #up to it from the start URL recorded
+                if neighbor in links.keys():
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+                    paths[neighbor] = paths[current] + f" -> {neighbor}"
     return False
 """
 if __name__ == "__main__":
--- BeautifulSoup/depthFirstSearch.py
+++ BeautifulSoup/depthFirstSearch.py
@@ -9,19 +9,23 @@ def dfs(links, start, target):
     result = dfs_helper(links, start, target, stack, visited)
 
     if result:
+        print(f"Depth First Search:")
         print(f"The shortest path between {start} and {target} is:", end = " ")
+        #Prints out the URLs that lead from the start URL to the final URL
         first = stack.pop()
         print(f"{first}", end = " ")
         while len(stack) > 0:
             current = stack.pop()
             print(f"-> {current}", end = " ")
-        t = timeit.Timer(lambda: dfs_helper(links, current, target, stack, visited))
+        #Prints out the average of the DFS function running for 100 times
+        t = timeit.Timer(lambda: dfs_helper(links, start, target, stack, visited))
         print("")
         print("Total time taken: ", t.timeit(100), "seconds")
     else:
+        #Reports if the search was unsuccessful, as well as the average time of the function
+        print(f"Depth First Search Failed:")
         print(f"There is no path between {start} and {target}")
-        t = timeit.Timer(lambda: dfs_helper(links, current, target, stack, visited))
-        print("")
+        t = timeit.Timer(lambda: dfs_helper(links, start, target, stack, visited))
         print("Total time taken: ", t.timeit(100), "seconds")
 
     return result
@@ -31,14 +35,19 @@ def dfs(links, start, target):
 def dfs_helper(links, article, target, stack, visited):
     visited.add(article)
     if article == target:
+        #When the target URL is found, it is added to the stack
         stack.append(article)
         return True
     for neighbor in links[article]:
-        if neighbor not in visited:
-            visited.add(neighbor)
-            if dfs_helper(links, neighbor, target, stack, visited):
-                stack.append(article)
-                return True
+        if neighbor in links.keys():
+            if neighbor not in visited:
+                visited.add(neighbor)
+                if dfs_helper(links, neighbor, target, stack, visited):
+                    #Once the target URL is found, all the URLs leading up to it are added recursively,
+                    #So the final URL is added first and the start URL is added last
+                    if article in links.keys():
+                        stack.append(article)
+                        return True
     return False
 
 """
